a6ac4722794c77d9e84f570682239eddf882cc86
FM1S <38359240+FM1S@users.noreply.github.com>
2019-09-27 20:14:04 +0300

    build commit

---
+++ build/precache-manifest.23aa834d6efde66c7bec2577913229f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "51f02b5214f5e4fb54c5",
+    "url": "/static/css/main.c268d3d7.chunk.css"
+  },
+  {
+    "revision": "51f02b5214f5e4fb54c5",
+    "url": "/static/js/main.146c3465.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e405533193b5e9ec5a1a",
+    "url": "/static/css/2.7da57942.chunk.css"
+  },
+  {
+    "revision": "e405533193b5e9ec5a1a",
+    "url": "/static/js/2.7a838f1b.chunk.js"
+  },
+  {
+    "revision": "597b5fc665fa7a1dfeb4918036355361",
+    "url": "/index.html"
+  }
+];
